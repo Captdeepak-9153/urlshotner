bcbbd6aa76d215afc58d666366beeb4fc7324729
Deepak kumar kar <94552919+Captdeepak-9153@users.noreply.github.com>
2024-02-06 20:02:04 +0530

    intial changes in jwt authenticatio

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,19 @@
-const sessionIdToUserMap = new Map();// this is a hashmap
-
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken");
+const secret = "Deepak@9153";
+function setUser(user) {
+  return jwt.sign({
+    _id: user._id,
+    email:user.email,
+  },secret);
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if(!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
