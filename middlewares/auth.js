9cadb353e9909a357870d4dbc32e02f4f90418a4
Deepak kumar kar <deepakkumarkar129@gmail.com>
2024-06-27 10:56:08 +0530

    push

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,19 @@
 const { getUser } = require("../service/auth");
+//AUTHENTICATION
+
+// function checkForAuthentication (req ,res , next) {
+//   const authorizationHeaderValue = req.header["authorization"];
+//   req.user = null;
+//   if (!authorizationHeaderValue || authorizationHeaderValue.startsWith('Bearer'))
+//   return next();
+  
+//   const token = authorizationHeaderValue.split("Bearer") [1];
+//   getUser();
+// }
+
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.cookies?.uid; // this will check whether there is a cookie in the url which send by browser
 
   if (!userUid) return res.redirect("/login");
   const user = getUser(userUid);
